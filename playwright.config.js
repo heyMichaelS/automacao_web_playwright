6c04605d033f2b0151d32559a0dbdf4a46ea608f
Michael Felipe <michaelfelipe180@gmail.com>
2025-02-11 02:54:14 -0300

    ajuste da url

--- playwright.config.js
+++ playwright.config.js
@@ -27,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    //  baseURL: 'https://automationpratice.com.br',
+    baseURL: 'https://automationpratice.com.br',
     baseUrl: 'https://my.api.org/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
